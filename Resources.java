b559ae6439055c4c279fc3518bc2e67e778c80a5
alienum <67610662+AL1ENUM@users.noreply.github.com>
2020-12-14 12:33:56 +0200

    Add files via upload

---
+++ Resources.java
@@ -0,0 +1,25 @@
+
+public class Resources {
+	
+	public Resources() {
+		
+	
+	}
+	
+	
+	public String getCipher() {
+		
+		return "al13njJ2Mrz4wjZDMSPwDr6TolQ==";
+	}
+
+	public String getSecret() {
+		
+		return "al13n/aom7EHcuiCWzNArA72UVn0nnVtJ5jZSPHDmjFPc5KQ=";
+	}
+	
+	public String gotSecret() {
+		
+		return "pr0tect1on1smust";
+	}
+	
+}
